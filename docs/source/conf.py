0ed73d1c33533cdc3f8d2ba915751ef8e4539f3f
Ross Motley <ross.motley@gmail.com>
2022-11-28 18:49:49 +0000

    Trying a nicer theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,9 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "furo"
+
+html_logo = "images/logo.png"
 
 html_theme_options = {
     "analytics_anonymize_ip": True,
