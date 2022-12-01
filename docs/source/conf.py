73ff5de488c7913193c45f1fdcdd8bbd92a86f4d
Ross Motley <ross.motley@chipflow.io>
2022-12-01 17:15:24 +0000

    Add sphinx_copybutton to docs (ChipFlow/chipflow-docs!9)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'myst_parser'
+    'myst_parser',
+    'sphinx_copybutton',
 ]
 
 intersphinx_mapping = {
