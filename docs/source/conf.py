7d4fc1945d0d8187449af463af27c8202c8bb62a
Ross Motley <ross.motley@chipflow.io>
2022-12-01 17:09:30 +0000

    Change theme to furo (ChipFlow/chipflow-docs!5)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,10 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "furo"
+
+# Logo is not from `_static`, it gets copied.
+html_logo = "logo.png"
 
 html_theme_options = {
     "analytics_anonymize_ip": True,
