83bb1f4dd8da7ebe614b8e3041a10395b2af89e6
Rob Taylor <rob.taylor@chipflow.io>
2025-04-28 22:20:49 +0100

    Rework documentation
    This reworks the documentation to include amaranth, amaranth-soc, and
    chipflow-lib and struture together well.
    
    Updates the CI to have deployments on chipflow-infra.com, with pr
    reviews. Releases deploy to docs.chipflow.io

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,5 @@
 # Configuration file for the Sphinx documentation builder.
-
+from pathlib import Path
 # -- Project information
 
 project = 'ChipFlow'
@@ -10,22 +10,55 @@ release = 'alpha'
 version = '0.1.0'
 
 # -- General configuration
-
+top_dir = Path(__file__).parent / ".." / ".."
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
-    'myst_parser',
+    'sphinx.ext.autosummary',
     'sphinx_copybutton',
+    'myst_parser',
+    'sphinx.ext.todo',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx_rtd_theme',
+    'sphinxcontrib.platformpicker',
+    'sphinxcontrib.yowasp_wavedrom',
+    'sphinx.ext.autodoc',
+    'sphinxext.rediraffe',
 ]
+"""
+    "sphinx_design",
+    "sphinx_design_elements",
+"""
+
+rst_prolog = """
+.. role:: amaranth
+
+.. role:: amaranth-soc
+
+.. role:: chipflow-lib
+
+.. role:: py(code)
+   :language: python
+"""
+
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
 intersphinx_disabled_domains = ['std']
+
+exclude_patterns = [
+        'amaranth/index.rst',
+        'amaranth/cover.rst',
+        'amaranth-soc/index.rst',
+        'amaranth-soc/cover.rst',
+        ]
+
+rediraffe_redirects = {
+    "simulator.rst": "amaranth/simulator.rst",
+}
 
 templates_path = ['_templates']
 
@@ -36,24 +69,10 @@ html_theme = "furo"
 # Favicon is not from `_static`, it gets copied:
 html_favicon = "favicon.png"
 
-html_theme_options = {
-    "light_logo": "images/logo.png",
-    "dark_logo": "images/logo-dark-mode.png",
-    "analytics_anonymize_ip": True,
-    "light_css_variables": {
-        "font-stack": "Poppins, Verdana, sans-serif",
-        "color-brand-primary": "#7C4FA0",
-        "color-brand-content": "#7C4FA0",
-    },
-    "sidebar_hide_name": True,
-}
-
 # These folders are copied to the documentation's HTML output
 html_static_path = ["_static"]
 
-html_css_files = [
-    'css/custom.css',
-]
+html_logo = "_static/logo.png"
 
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
@@ -63,3 +82,42 @@ html_js_files = [
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+autodoc_member_order = "bysource"
+autodoc_default_options = {
+    "members": True,
+    'ignore-module-all': True
+}
+autodoc_preserve_defaults = True
+autodoc_inherit_docstrings = False
+
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+napoleon_use_ivar = True
+napoleon_include_init_with_doc = True
+napoleon_include_special_with_doc = True
+napoleon_custom_sections = [
+    ("Attributes", "params_style"), # by default displays as "Variables", which is confusing
+    ("Members", "params_style"), # `lib.wiring` signature members
+    "Platform overrides"
+]
+
+
+linkcheck_ignore = [
+    r"^http://127\.0\.0\.1:8000$",
+    # Picked up automatically by ReST and doesn't have an index.
+    r"^https://amaranth-lang\.org/schema/$",
+]
+
+linkcheck_anchors_ignore_for_url = [
+    r"^https://matrix\.to/",
+    r"^https://web\.libera\.chat/",
+    # React page with README content included as a JSON payload.
+    r"^https://github\.com/[^/]+/[^/]+/$",
+]
+
+
+# Silence the warnings globally; otherwise they may fire on object destruction and crash completely
+# unrelated tests.
+import amaranth._unused
+amaranth._unused.MustUse._MustUse__silence = True
